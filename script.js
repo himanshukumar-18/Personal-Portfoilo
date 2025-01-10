1ed834efce9dda69dadc15d7f372378b2c5e1a20
frontenddev-himanshu <rajh5343@gmail.com>
2025-01-10 22:11:52 +0530

    add gsap

--- script.js
+++ script.js
@@ -228,6 +228,26 @@ resumeBtn.addEventListener("click", () => {
         wordPoint.style.opacity = 1;
 
 
-    }, 1000);
+    }, 2000);
 
 })
+
+
+
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#page2",
+        start: "40% 90%",
+        end: "200% 90%",
+        // markers: true,
+        scrub: true,
+    }
+});
+
+tl.to(".second", {
+    x: 450,
+}, 'a')
+tl.to(".first", {
+    x: -450,
+}, 'a')
