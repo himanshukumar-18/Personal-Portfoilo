78553627d72eed08efc0a8ad96a2f12f38fd598b
frontenddev-himanshu <rajh5343@gmail.com>
2025-01-12 19:11:48 +0530

    add

--- script.js
+++ script.js
@@ -238,8 +238,8 @@ resumeBtn.addEventListener("click", () => {
 let tl = gsap.timeline({
     scrollTrigger: {
         trigger: "#page2",
-        start: "40% 90%",
-        end: "200% 90%",
+        start: "40% 100%",
+        end: "300% 100%",
         // markers: true,
         scrub: true,
     }
@@ -251,3 +251,61 @@ tl.to(".second", {
 tl.to(".first", {
     x: -450,
 }, 'a')
+
+
+
+
+
+
+// swiper
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var appendNumber = 4;
+var prependNumber = 1;
+document
+    .querySelector(".prepend-2-slides")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.prependSlide([
+            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+        ]);
+    });
+document
+    .querySelector(".prepend-slide")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.prependSlide(
+            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+        );
+    });
+document
+    .querySelector(".append-slide")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.appendSlide(
+            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+        );
+    });
+document
+    .querySelector(".append-2-slides")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.appendSlide([
+            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+        ]);
+    });
